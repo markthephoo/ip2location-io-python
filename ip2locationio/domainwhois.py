0ec2cc4d7fed15763dfe737f260d46b4a5d76cb0
markthephoo <mark.p.phubet@gmail.com>
2025-01-28 10:07:34 +0700

    Update domainwhois.py

--- ip2locationio/domainwhois.py
+++ ip2locationio/domainwhois.py
@@ -91,3 +91,67 @@ class DomainWHOIS:
   
 class IP2LocationIOAPIError(Exception):
     """Raise for IP2Location API Error Message"""
+{
+    "domain": "locaproxy.com",
+    "domain_id": "1710914405_DOMAIN_COM-VRSN",
+    "status": "clientTransferProhibited https://icann.org/epp#clientTransferProhibited",
+    "create_date": "2012-04-03T02:34:32Z",
+    "update_date": "2021-12-03T02:54:57Z",
+    "expire_date": "2024-04-03T02:34:32Z",
+    "domain_age": 3863,
+    "whois_server": "whois.godaddy.com",
+    "registrar": {
+        "iana_id": "146",
+        "name": "GoDaddy.com, LLC",
+        "url": "https://www.godaddy.com"
+    },
+    "registrant": {
+        "name": "Registration Private",
+        "organization": "Domains By Proxy, LLC",
+        "street_address": "DomainsByProxy.com",
+        "city": "Tempe",
+        "region": "Arizona",
+        "zip_code": "85284",
+        "country": "US",
+        "phone": "+1.4806242599",
+        "fax": "+1.4806242598",
+        "email": "Select Contact Domain Holder link at https://www.godaddy.com/whois/results.aspx?domain=LOCAPROXY.COM"
+    },
+    "admin": {
+        "name": "Registration Private",
+        "organization": "Domains By Proxy, LLC",
+        "street_address": "DomainsByProxy.com",
+        "city": "Tempe",
+        "region": "Arizona",
+        "zip_code": "85284",
+        "country": "US",
+        "phone": "+1.4806242599",
+        "fax": "+1.4806242598",
+        "email": "Select Contact Domain Holder link at https://www.godaddy.com/whois/results.aspx?domain=LOCAPROXY.COM"
+    },
+    "tech": {
+        "name": "Registration Private",
+        "organization": "Domains By Proxy, LLC",
+        "street_address": "DomainsByProxy.com",
+        "city": "Tempe",
+        "region": "Arizona",
+        "zip_code": "85284",
+        "country": "US",
+        "phone": "+1.4806242599",
+        "fax": "+1.4806242598",
+        "email": "Select Contact Domain Holder link at https://www.godaddy.com/whois/results.aspx?domain=LOCAPROXY.COM"
+    },
+    "billing": {
+        "name": "",
+        "organization": "",
+        "street_address": "",
+        "city": "",
+        "region": "",
+        "zip_code": "",
+        "country": "",
+        "phone": "",
+        "fax": "",
+        "email": ""
+    },
+    "nameservers": ["vera.ns.cloudflare.com", "walt.ns.cloudflare.com"]
+}
